00588361307c2e60223179c8fabfb56e52780a8e
palles77 <leslaw.pawlaczyk@recogify.com>
2023-03-22 22:53:53 +0100

    Exercise 1

---
+++ Exercise_1/SampleHierarchies.App/SampleHierarchies.Gui/MainScreen.cs
@@ -0,0 +1,87 @@
+﻿using SampleHierarchies.Enums;
+using SampleHierarchies.Interfaces.Services;
+
+namespace SampleHierarchies.Gui;
+
+/// <summary>
+/// Application main screen.
+/// </summary>
+public sealed class MainScreen : Screen
+{
+    #region Properties And Ctor
+
+    /// <summary>
+    /// Data service.
+    /// </summary>
+    private IDataService _dataService;
+
+    /// <summary>
+    /// Animals screen.
+    /// </summary>
+    private AnimalsScreen _animalsScreen;
+
+    /// <summary>
+    /// Ctor.
+    /// </summary>
+    /// <param name="dataService">Data service reference</param>
+    /// <param name="animalsScreen">Animals screen</param>
+    public MainScreen(
+        IDataService dataService,
+        AnimalsScreen animalsScreen)
+    {
+        _dataService = dataService;
+        _animalsScreen = animalsScreen;
+    }
+
+    #endregion Properties And Ctor
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public override void Show()
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Your available choices are:");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Animals");
+            Console.WriteLine("2. Create a new settings");
+            Console.Write("Please enter your choice: ");
+
+            string? choiceAsString = Console.ReadLine();
+
+            // Validate choice
+            try
+            {
+                if (choiceAsString is null)
+                {
+                    throw new ArgumentNullException(nameof(choiceAsString));
+                }
+
+                MainScreenChoices choice = (MainScreenChoices)Int32.Parse(choiceAsString);
+                switch (choice)
+                {
+                    case MainScreenChoices.Animals:
+                        _animalsScreen.Show();
+                        break;
+
+                    case MainScreenChoices.Settings:
+                        Console.WriteLine("Not yet implemented.");
+                        // TODO: implement
+                        break;
+
+                    case MainScreenChoices.Exit:
+                        Console.WriteLine("Goodbye.");
+                        return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    #endregion // Public Methods
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ProgrammingObjectOriented
-Object oriented programming exercises for students in WSB
+# Object Oriented Programming
+
+This a repo with exercises for students at WSB in Chorzów to practice object oriented programming in C#
+
+1. Exercise 1 - basic data types and inheritance
+
+# Last Modified
+2023/03/22
